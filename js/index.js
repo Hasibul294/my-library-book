0835c60e4aa68bc76d8e2f806464862b93b09bc5
Hasibul294 <hasibulhasan294@gmail.com>
2021-09-02 16:46:42 +0600

    update total result

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const inputField = document.getElementById('input-field');
 const searchButton = document.getElementById('search-button');
-const resultField = document.getElementById('result-field');
+const resultCounter = document.getElementById('total-result');
+const resultField = document.getElementById('result-field');    
 
 const loadBook = () => {
     const url = `http://openlibrary.org/search.json?q=${inputField.value}`;
@@ -12,27 +13,30 @@ const loadBook = () => {
 
 const displaySearchResult = books => {
     resultField.textContent = '';
-    // console.log(books.docs);
     let count = 0;
-    resultField.textContent = '';
-    books.forEach(book => {
-        // console.log(book.cover_i);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top h-50" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">Author: ${book.author_name?.[0]}</p>
-                    <p class="card-text">1st Publish Year: ${book.first_publish_year}</p>
-                    <p class="card-text">Publisher: ${book.publisher?.[0]}</p>
+    if(books.length === 0){
+        resultCounter.innerText = 'No result Found';
+    }
+    else{
+        books.forEach(book => {
+            resultCounter.textContent = '';
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100">
+                    <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top h-50" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">Author: ${book.author_name?.[0]}</p>
+                        <p class="card-text">1st Publish Year: ${book.first_publish_year}</p>
+                        <p class="card-text">Publisher: ${book.publisher?.[0]}</p>
+                    </div>
                 </div>
-            </div>
-        `;
-        resultField.appendChild(div);
-        count++;
-        console.log(count);
-    })
+            `;
+            resultField.appendChild(div);
+            count++;
+        })
+        resultCounter.innerText = `Total Result: ${count}`;
+    }
     
 }
